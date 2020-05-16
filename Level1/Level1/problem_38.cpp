c55ef1a8e8c0d87b01f572032e0818fcaa51d9a6
JINKOO <jacob456@hanmail.net>
2020-05-16 20:42:47 +0900

    source file added

--- Level1/Level1/problem_38.cpp
+++ Level1/Level1/problem_38.cpp
@@ -51,36 +51,96 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int solution(string dartResult) 
 {
     int answer = 0;
     //
-    int start = 0;
-    int pos;
-    vector<string> v;
-    for (int i = 0; i < dartResult.length(); i++)
+    cout << dartResult << "\n";
+    vector<int> numeric;
+    vector<char> bonus;
+    vector<char> option;
+  
+    for (int i = 0; i < dartResult.size(); i++)
     {
-        string str = "";
-        if(dartResult[i % 3] == '*' || dartResult[i] == '#')
+        if (dartResult[i] >= 'A' && dartResult[i] <= 'Z')
+        {
+            bonus.push_back(dartResult[i]);
+            if (dartResult[i + 1] == '*')
+                option.push_back('*');
+            else if (dartResult[i + 1] == '#')
+                option.push_back('#');
+            else
+                option.push_back(' ');
+        }
+        else if (dartResult[i] >= '0' && dartResult[i] <= '9')
+        {
+            if (dartResult[i + 1] == '0')
+            {
+                numeric.push_back(10);
+                i++;
+            }
+            else
+                numeric.push_back(dartResult[i] - '0');
+        }
     }
+
+    int digit = 0;
+    vector<int> sub_score;
+
+    for (int i = 0; i < 3; i++)
+    {
+        digit = numeric[i];
+        
+        if (bonus[i] == 'S')
+            digit = digit * 1;
+        else if (bonus[i] == 'D')
+            digit = digit * digit;
+        else if (bonus[i] == 'T')
+            digit = digit * digit * digit;
+        
+    
+        if (option[i] == '*')
+        {
+            sub_score.push_back(digit * 2);
+            if (i >= 1)
+                sub_score[i-1] *= 2;
+        }
+        else if (option[i] == '#')
+            sub_score.push_back(digit * -1);
+        else if (option[i] == ' ')
+            sub_score.push_back(digit);
+    }
+
+    for (int i = 0; i < sub_score.size(); i++)
+    {
+        //cout << sub_score[i] << " ";
+        answer += sub_score[i];
+    }
+    cout << "\n";
     //
     return answer;
 }
 
 void printResult(int answer)
 {
-    cout << answer << "\n";
+    cout <<"answer :: " << answer << "\n";
 }
 
 int main()
 {
 
     printResult(solution("1S2D*3T"));
+    printResult(solution("1D2S#10S"));
+    printResult(solution("1D2S0T"));
     printResult(solution("1S*2T*3S"));
     printResult(solution("1D#2S*3S"));
-
+    printResult(solution("1T2D3D#"));
+    printResult(solution("1D2S3T*"));
+    printResult(solution("1D#10S*2S*"));
     
+
     return 0;
 }
