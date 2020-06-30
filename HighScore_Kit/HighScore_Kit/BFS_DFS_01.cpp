92daaa4fc7ac4b76a5305647f7713b16ff51b4a6
JINKOO <jacob456@hanmail.net>
2020-06-30 23:06:12 +0900

    source file added

--- HighScore_Kit/HighScore_Kit/BFS_DFS_01.cpp
+++ HighScore_Kit/HighScore_Kit/BFS_DFS_01.cpp
@@ -23,3 +23,38 @@
              numbers	target	  return
      [1, 1, 1, 1, 1]	     3	       5
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int answer = 0;
+
+void dfs(int index, vector<int> numbers, int target, int total)
+{
+    if (index == numbers.size())
+    {
+        if (total == target)
+            answer++;
+        return;
+    }
+
+    dfs(index + 1, numbers, target, total + numbers[index]);
+    dfs(index + 1, numbers, target, total + numbers[index] * -1);
+}
+
+int solution(vector<int> numbers, int target) 
+{
+    //
+    dfs(0, numbers, target, 0);
+    //
+    return answer;
+}
+
+int main()
+{
+    vector<int> v = { 1,1,1,1,1 };
+    cout << solution(v, 3) << "\n";
+
+    return 0;
+}
