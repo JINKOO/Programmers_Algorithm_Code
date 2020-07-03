f830a68a7360c08b41821e76dc9d8204119042e6
JINKOO <jacob456@hanmail.net>
2020-07-03 18:37:32 +0900

    source file added

---
+++ Level2/Level2/problem_06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    
+}
+
+int main()
+{
+
+    cout << solution({ {0, 1, 1, 1},{1, 1, 1, 1},{1, 1, 1, 1},{0, 0, 1, 0} }) << "\n";
+
+    return 0;
+}
