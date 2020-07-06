a659b89a3ae4366d1ffa2a0668ab6edf2eee2d18
JINKOO <jacob456@hanmail.net>
2020-07-06 16:44:26 +0900

    source file added

---
+++ Level2/Level2/problem_08.cpp
@@ -0,0 +1,127 @@
+/*
+  #. [ 소수 찾기 ]
+  #. 문제 설명
+     한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
+     각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 
+     return 하도록 solution 함수를 완성해주세요.
+
+  #. 제한사항
+     - numbers는 길이 1 이상 7 이하인 문자열입니다.
+     - numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+     - 013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+ 
+  #. 입출력 예
+     numbers	return
+          17	     3
+         011         2
+
+  #. 입출력 예 설명
+     - 예제 #1
+       [1, 7]으로는 소수 [7, 17, 71]를 만들 수 있습니다.
+
+     - 예제 #2
+       [0, 1, 1]으로는 소수 [11, 101]를 만들 수 있습니다.
+       11과 011은 같은 숫자로 취급합니다.
+*/
+
+/*
+  #. 나의 접근법.
+     - numbers문자열을 하나씩 쪼개서 모든 경우의 수를 통하여 정수로 변환 후,
+       vector<int>에 넣어서 소수인지 판별하려고 함. --> 실패.
+
+  #. 풀이
+     - '완전 탐색' 문제이다. 완전 탐색을 어느 부분에 적용시킬 것인지.
+     - 우선 numbers에 있는 숫자문자로 만들 수 있는 최대 정수를 만들고,
+       1. 2 ~ max까지 반복하면서,
+       2. 소수인지 판별하고,
+       3. 해당 소수가 기존 numbers에 포함된 문자인지 판별한다.
+
+       ==> 1 ~ 3 부분이 완전탐색..
+*/
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+//소수 판별에 있어서 가장 빠른 방법이다.
+//에라토스테네스의 체보다 빠른 듯..??
+bool isPrime(int n)
+{
+    if (n == 0)
+        return false;
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool contain(string numbers, int n)
+{
+    string s = to_string(n);
+    //1. count가 s의 length()와 같다는 의미 = 해당 소수인 n이 numbers에 모두 포함되어있다.
+    int count = 0;
+
+    //2. numbers와 s의 각 index를 비교하는데, 비교한 자리인지 판단하기 위하여, 
+    vector<bool> check(numbers.length(), false);
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = 0; j < numbers.length(); j++)
+        {
+            if (s[i] == numbers[j] && check[j] == false)
+            {
+                check[j] = true;
+                count++;
+                // break를 거는 이유 : 일단 같은 거 찾으면 break함으로써, 
+                // 11과 같은 n이 넘어왔을 때, 앞에 1과 뒤에 1이 동시에 count되는 것을 막는다. 
+                break;
+            }
+        }
+    }
+    //3. count가 s의 length()와 같다는 의미 = 해당 소수인 n이 numbers에 모두 포함되어있다.
+    return count == s.length() ? true : false;
+}
+
+bool myCompare(char c1, char c2) 
+{
+    return c1 > c2;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    //
+    //1. numbers에서 가장 최댓값을 찾는다.
+    int max = 0;
+    //1.1 최댓값을 쉽게 찾기 위해 내림차순 정렬한다.
+    sort(numbers.begin(), numbers.end(), myCompare);
+    max = stoi(numbers);
+
+    //2. 2 ~ max까지 소수 찾고, 이 소수가 기존 numbers에 들어있나 check한다.
+    for (int i = 2; i <= max; i++)
+    {
+        //2.2 소수이면,
+        if (isPrime(i))
+        {
+            //2.3 해당 소수가, numbers에 포함되어있는 문자인지 check한다.
+            if (contain(numbers, i))
+                answer++;
+        }
+    }
+    //
+    return answer;
+}
+
+int main()
+{
+    cout << solution("17") << "\n";
+    cout << solution("011") << "\n";
+    cout << solution("012342") << "\n";
+
+    return 0;
+}
---
+++ Level2/Level2/problem_09.cpp
@@ -0,0 +1,124 @@
+/*
+  #. [ 프로그래머스_더 맵게 ]
+  
+  #. 문제 설명
+     매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 
+     모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+     
+     섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+     
+     Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+     Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 
+     모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+
+  #. 제한 사항
+     - scoville의 길이는 2 이상 1,000,000 이하입니다.
+     - K는 0 이상 1,000,000,000 이하입니다.
+     - scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+     - 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+
+  #. 입출력 예
+                scoville	K	return
+     [1, 2, 3, 9, 10, 12]	7	     2
+
+  #. 입출력 예 설명
+     스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+     새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
+     가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
+     
+     스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+     새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
+     가진 음식의 스코빌 지수 = [13, 9, 10, 12]
+     
+     모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
+*/
+
+/*
+   #. 나의 접근법.
+      - 초반에 queue를 사용해서 풀려고 하였다.
+      - 예전 자료구조때 heap은 우선순위 큐를 사용하여 정렬상태를 유지한다는 생각이나서
+        우선순위 큐 사용.
+      - 우선 순위 큐를 사용함으로써, 정렬 상태를 유지 할 수 있다. 여기서는 오름차순..
+        scoville의 원소들을 우선순위 큐에 삽입한다.
+
+        이후, pq의 top원소와 K를 비교하여, 
+        1. K보다 크거나 같으면 그냥 pop하고,
+        2. 아니면 mix해야한다. 이때, pop을 한번 더한다. 또 이때, pq가 empty이면, 더 이상
+           K이상으로 만들 수 없는 음식이다. 따라서 -1 return한다.
+
+   #. prioity_queue는 정렬 상태를 유지한다. 이를 명심하자.
+   #. 문제를 보고 queue가 아닌, priority_queue를 사용해야하는 이유...
+*/
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int mix(int food1, int food2)
+{
+    return food1 + (food2 * 2);
+}
+
+/*
+void print(priority_queue<int, vector<int>, greater<int>> pq)
+{
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << "\n";
+}
+*/
+
+int solution(vector<int> scoville, int K) 
+{
+    int answer = 0;
+    //
+    //우선순위 큐를 사용하여, 정렬 상태를 유지한다.
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < scoville.size(); i++)
+        pq.push(scoville[i]);
+
+    while (!pq.empty())
+    {
+        //top값 임시저장
+        int temp = pq.top();
+        //이 값이 K보다 크면 pop
+        if (temp >= K)
+            pq.pop();
+        else
+        {
+            //우선 pop
+            pq.pop();
+            //pop했는데, 큐가 비어있다는 것은, K이상인 값으로 만들 수 없다는 의미
+            //즉 더이상 mix연산을 진행할 수 없음.
+            if (!pq.empty())
+            {
+                int new_s = mix(temp, pq.top());
+                pq.pop();
+                answer++;
+                pq.push(new_s); //이때, 맨 뒤에 삽입이 아니라, 정렬 상태(오름차순)를 맞춰 삽입된다.
+                //print(pq);
+            }
+
+            else
+                return -1;
+        }
+    }
+    //
+    return answer;
+}
+
+int main()
+{
+
+    vector<int> scoville = { 1, 2, 3, 9, 10, 12 };
+    int K = 7;
+    cout << solution(scoville, K) << "\n";
+    cout << solution(scoville, 1000000000) << "\n";
+
+    return 0;
+}
